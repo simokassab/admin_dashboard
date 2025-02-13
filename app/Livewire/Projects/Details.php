ae84b1cfe4dfb30f1ed3d2f6abbe2b3e5d537136
Mohammad KASSAB <mohammad.a.kassab@gmail.com>
2025-02-13 11:48:51 +0200

    fix

--- app/Livewire/Projects/Details.php
+++ app/Livewire/Projects/Details.php
@@ -10,13 +10,14 @@ use Livewire\Component;
 class Details extends Component
 {
     public ProjectSource $project;
-    public $trackingData, $visits, $first_clicks, $second_clicks, $successes, $failures;
+    public $trackingData, $visits, $first_clicks, $second_clicks, $successes, $failures, $second_visit;
 
     public function mount(ProjectSource $project)
     {
         $this->project = $project;
         $this->trackingData = Tracking::where('project_source_id', $project->id)->get();
         $this->visits = $this->trackingData->count();
+        $this->second_visit = $this->trackingData->where('second_page_visit', '1')->count();
         $this->first_clicks = $this->trackingData->where('first_click', '1')->count();
         $this->second_clicks = $this->trackingData->where('second_click', '1')->count();
         $this->successes = $this->trackingData->where('success', '1')->count();
--- resources/views/livewire/projects/details.blade.php
+++ resources/views/livewire/projects/details.blade.php
@@ -16,7 +16,7 @@
                         </div>
 
                     </div>
-                    <section class="grid md:grid-cols-5 grid-cols-5 gap-6">
+                    <section class="grid md:grid-cols-6 grid-cols-6 gap-6">
                         <div class="flex items-center p-8 bg-white shadow rounded-lg dark:bg-gray-600">
                             <div class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-blue-600 bg-blue-100 rounded-full mr-6">
                                 <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
@@ -26,6 +26,17 @@
                             <div>
                                 <span class="block text-2xl font-bold dark:text-gray-200">{{ $visits }}</span>
                                 <span class="block text-gray-500 dark:text-gray-200"> Visits</span>
+                            </div>
+                        </div>
+                        <div class="flex items-center p-8 bg-white shadow rounded-lg dark:bg-gray-600">
+                            <div class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-blue-600 bg-blue-100 rounded-full mr-6">
+                                <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                                </svg>
+                            </div>
+                            <div>
+                                <span class="block text-2xl font-bold dark:text-gray-200">{{ $second_visit }}</span>
+                                <span class="block text-gray-500 dark:text-gray-200"> Second page Visit</span>
                             </div>
                         </div>
                         <div class="flex items-center p-8 bg-white shadow rounded-lg dark:bg-gray-600">
